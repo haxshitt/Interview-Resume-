902bc28f9b5f97c67f601caa08d27f76cb194279
Harshit Pathak <try.harshitt@gmail.com>
2026-04-19 03:55:01 +0530

    cors issue solved

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,8 @@ app.use(express.json())
 app.use(cookieParser())
 const allowedOrigins = [
     "http://localhost:5173",
+    "http://localhost:3000",
+    "https://interview-resume-gamma.vercel.app",
     process.env.FRONTEND_URL
 ];
 
